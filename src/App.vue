5fc3d6b85c119546aeb9cc30bfda5330848953cf
Xun Qiao <xun.qiao@microfocus.com>
2019-05-04 10:03:32 +0800

    edit config.js

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,11 @@ import Home from './views/Home'
 export default {
   components: {
     Home
+  },
+  mounted() {
+    // img not found at Github Pages
+    const { NODE_ENV } = process.env;
+    document.documentElement.className = NODE_ENV;
   }
 }
 
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/elementui_draggable_example" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "./static" : "/"
 };
